c186bf4f6a2a1d415de29d83846cf75f21e11411
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-10-24 18:48:37 +0530

    created a database movieTickets.db and created a table user and done sample queries

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,5 +1,39 @@
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const cors = require("cors");
+const db = new sqlite3.Database(
+  "./movieTickets.db",
+  sqlite3.OPEN_READWRITE,
+  (error) => {
+    if (error) return console.error(error.message);
+  }
+);
+let sql;
+// Creating a table user
+// let sql =
+//   "CREATE TABLE user(id INTEGER PRIMARY KEY,first_name,last_name,username,password,email)";
+// db.run(sql);
+
+// db.run("DROP TABLE user");
+// Inserting data into table user
+// sql =
+//   "INSERT INTO user(first_name,last_name,username,password,email)VALUES(?,?,?,?,?)";
+// db.run(
+//   sql,
+//   ["badrikumar", "gudisha", "badri", "gudishabadri", "badri@gmail.com"],
+//   (error) => {
+//     if (error) return console.error(error.message);
+//   }
+// );
+
+sql = "SELECT * FROM user";
+db.all(sql, [], (error, rows) => {
+  if (error) return console.error(error.message);
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
+
 const app = express();
 
 app.use(cors());
